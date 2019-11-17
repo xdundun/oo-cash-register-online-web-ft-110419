2f1966cc26c6c3f9a8340f149d19709446659b96
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-17 18:09:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
 
   def add_item(item,cost,quantity=1)
     @total += cost * quantity
-    @items << item * quantity
+    @items << item[1...quantity]
   end
 
   def apply_discount
