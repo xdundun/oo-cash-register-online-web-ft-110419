820b637fb6efaca2cd2e22469b951d5ce2476546
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-17 18:48:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
 
   def apply_discount
       if @discount == 0
-        return "There is no discount to apply"
+        return "There is no discount to apply."
     else
 
       @total -= total * discount/100
