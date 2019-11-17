42136eea652747584544f873710acac616eb1bd4
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-17 18:54:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,7 @@ class CashRegister
     @total += cost * quantity
     @prices[:item] << cost
     @prices[:item] << quantity
-
+    puts @prices
       quantity.times do |q|
         @items << item
       end
