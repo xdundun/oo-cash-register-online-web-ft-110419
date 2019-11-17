8adea29e1ec5b691102915426e07a050be840603
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-17 18:06:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,6 +5,7 @@ class CashRegister
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+    @items = []
   end
 
   def add_item(item,cost,quantity=1)
@@ -12,5 +13,6 @@ class CashRegister
   end
 
   def apply_discount
+
   end
 end #end of class
